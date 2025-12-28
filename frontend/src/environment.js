d5817078437e14e06e2c6c2a78ba724298780226
jayramgit94 <155347360+jayramgit94@users.noreply.github.com>
2025-12-28 23:15:56 +0530

    Fix backend issues, local working

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = false;
 const server = IS_PROD
-  ? "https://zoom-call-yor3.onrender.com"
-  : "http://localhost:8002";
+  ? "https://zoom-zako.onrender.com"
+  : "http://localhost:8001";
 
 export default server;
